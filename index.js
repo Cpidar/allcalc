1f695a37f0054b939d922f58c33701429cbc4874
hoseinMo2 <h.moazeni92@gmail.com>
2018-03-15 14:08:46 +0330

    modify

--- index.js
+++ index.js
@@ -219,7 +219,7 @@ function startCalc(data, no, Ci, option = false) {
 
             i++;
             vorpal.ui.redraw(`${i} of ${alloys.valueOf()}`)
-            if ( i % no  == 0 ) {
+            if ( i % no  == 0 && src.length > 0) {
                 let str = JSON.stringify(src).slice(1, -1).concat(',')
                 // console.log(str)
                 fs.appendFileSync(`${filename}.json`, str, err => console.log(err));
